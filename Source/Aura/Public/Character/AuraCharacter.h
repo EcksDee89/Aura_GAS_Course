3d28fb8f5638b5bc613b938293a3b361478a42bf
EcksDee89 <sezan89@live.se>
2025-03-14 01:06:28 +0100

    Init Ability Actor Info

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -6,8 +6,6 @@
 #include "Character/AuraCharacterBase.h"
 #include "AuraCharacter.generated.h"
 
-
-
 /**
  * 
  */
@@ -17,5 +15,8 @@ class AURA_API AAuraCharacter : public AAuraCharacterBase
 	GENERATED_BODY()
 public:
 	AAuraCharacter();
-	
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
+private:
+	void InitAbilityActorInfo();
 };
