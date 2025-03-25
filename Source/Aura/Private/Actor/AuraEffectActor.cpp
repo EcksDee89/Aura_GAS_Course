7fc0ed7db8bf0e5f74870fd84fb866b9f642f3a5
EcksDee89 <sezan89@live.se>
2025-03-25 14:06:24 +0100

    Instant, Duration and Periodic Gameplay Effects + Effect Stacking

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -1,40 +1,33 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Actor/AuraEffectActor.h"
-#include "AbilitySystemComponent.h"
-#include "AbilitySystemInterface.h"
-#include "AbilitySystemBlueprintLibrary.h"
-#include "AbilitySystem/AuraAbilitySystemComponent.h"
-#include "AbilitySystem/AuraAttributeSet.h"
-#include "Components/SphereComponent.h"
-
-AAuraEffectActor::AAuraEffectActor()
-{
-	PrimaryActorTick.bCanEverTick = false;
-
-	SetRootComponent (CreateDefaultSubobject<USceneComponent>("SceneRoot"));
-
-}
-
-
-
+ 
+ #include "AbilitySystemBlueprintLibrary.h"
+ #include "AbilitySystemComponent.h"
+ 
+ AAuraEffectActor::AAuraEffectActor()
+ {
+ 	PrimaryActorTick.bCanEverTick = false;
+ 
+ 	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
+ }
+ 
 void AAuraEffectActor::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
+ {
+ 	Super::BeginPlay();
+ 
+ }
 
-void AAuraEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
-{
-	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
-	if (TargetASC == nullptr) return;
-
-	check(GameplayEffectClass);
-	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
-	EffectContextHandle.AddSourceObject(this);
-	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
-	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
-}
+void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+ {
+ 	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+ 	if (TargetASC == nullptr) return;
+ 
+ 	check(GameplayEffectClass);
+ 	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+ 	EffectContextHandle.AddSourceObject(this);
+ 	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
+ 	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+ }
 
 
